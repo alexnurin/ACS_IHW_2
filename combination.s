66f1b66ae9913f78d801d1c11ccc64dd71ae8d3d
alexnurin <an362@mail.ru>
2022-11-13 22:31:42 +0300

    асм модификация: удаление ненужного

--- combination.s
+++ combination.s
@@ -4,7 +4,6 @@
 	.globl	combination
 	.type	combination, @function
 combination:
-	endbr64
 	push	rbp
 	mov	rbp, rsp
 	push	rbx
@@ -117,7 +116,6 @@ combination:
 	add	DWORD PTR -48[rbp], 1
 .L10:
 	mov	eax, DWORD PTR -48[rbp]
-	cdqe
 	cmp	QWORD PTR -24[rbp], rax    # for (int j = 0; j < cur_size; j++) {
 	ja	.L13
 .L12:
@@ -153,24 +151,3 @@ combination:
 	pop	rbx
 	pop	rbp
 	ret
-
-
-	.size	combination, .-combination
-	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
--- input.s
+++ input.s
@@ -4,7 +4,6 @@
 	.globl	input
 	.type	input, @function
 input:
-	endbr64
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 48
@@ -65,23 +64,3 @@ input:
 	mov	rax, QWORD PTR -24[rbp]
 	leave
 	ret
-
-	.size	input, .-input
-	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
