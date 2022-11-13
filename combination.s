a7e457ec11393e5027cb54d9393a70d1c5f2afed
alexnurin <an362@mail.ru>
2022-11-13 22:25:47 +0300

    асм комментарии

--- combination.s
+++ combination.s
@@ -9,8 +9,10 @@ combination:
 	mov	rbp, rsp
 	push	rbx
 	sub	rsp, 72
-	mov	QWORD PTR -72[rbp], rdi
-	mov	QWORD PTR -80[rbp], rsi
+	mov	QWORD PTR -72[rbp], rdi     # char *a
+	                                # a <=> -72[rbp]
+	mov	QWORD PTR -80[rbp], rsi     # char *b
+	                                # b <=> -80[rbp]
 	mov	rax, QWORD PTR -72[rbp]
 	mov	rdi, rax
 	call	strlen@PLT
@@ -20,85 +22,94 @@ combination:
 	call	strlen@PLT
 	add	rax, rbx
 	add	rax, 1
-	mov	QWORD PTR -56[rbp], rax
-	mov	QWORD PTR -24[rbp], 0
+	mov	QWORD PTR -56[rbp], rax     # size_t size = len(a) + len(b) + 1
+	                                # size <=> -56[rbp]
+	mov	QWORD PTR -24[rbp], 0       # size_t cur_size = 0
+	                                # cur_size <=> -24[rbp]
 	mov	rax, QWORD PTR -56[rbp]
 	mov	rdi, rax
 	call	malloc@PLT
-	mov	QWORD PTR -64[rbp], rax
-	mov	DWORD PTR -28[rbp], 0
+	mov	QWORD PTR -64[rbp], rax     # char *c = ..
+	                                # c <=> -64[rbp]
+	mov	DWORD PTR -28[rbp], 0       # int i = 0
+	                                # local i <=> -28[rbp]
 	jmp	.L2
 .L8:
-	mov	DWORD PTR -32[rbp], 0
-	mov	DWORD PTR -36[rbp], 0
+	mov	DWORD PTR -32[rbp], 0       # int found = 0
+	                                # found <=> -32[rbp]
+	mov	DWORD PTR -36[rbp], 0       # for (int j = 0;...
+	                                # local j <=> -36[rbp]
 	jmp	.L3
 .L6:
 	mov	eax, DWORD PTR -36[rbp]
 	movsx	rdx, eax
 	mov	rax, QWORD PTR -64[rbp]
-	add	rax, rdx
+	add	rax, rdx                    # c[j]
 	movzx	edx, BYTE PTR [rax]
 	mov	eax, DWORD PTR -28[rbp]
 	movsx	rcx, eax
 	mov	rax, QWORD PTR -72[rbp]
-	add	rax, rcx
+	add	rax, rcx                    # a[i]
 	movzx	eax, BYTE PTR [rax]
-	cmp	dl, al
+	cmp	dl, al                      # if (c[j] == a[i])
 	jne	.L4
-	mov	DWORD PTR -32[rbp], 1
-	jmp	.L5
+	mov	DWORD PTR -32[rbp], 1       # found = 1
+	jmp	.L5                         # break
 .L4:
 	add	DWORD PTR -36[rbp], 1
 .L3:
-	mov	eax, DWORD PTR -36[rbp]
+	mov	eax, DWORD PTR -36[rbp]     # for (..; j < ...) {
 	movsx	rbx, eax
-	mov	rax, QWORD PTR -64[rbp]
+	mov	rax, QWORD PTR -64[rbp]     # c
 	mov	rdi, rax
 	call	strlen@PLT
-	cmp	rbx, rax
+	cmp	rbx, rax                    # for (..; j < strlen(c); ..) {
 	jb	.L6
 .L5:
-	cmp	DWORD PTR -32[rbp], 0
+	cmp	DWORD PTR -32[rbp], 0       # if (!found)
 	jne	.L7
-	mov	eax, DWORD PTR -28[rbp]
+	mov	eax, DWORD PTR -28[rbp]     # i
 	movsx	rdx, eax
-	mov	rax, QWORD PTR -72[rbp]
-	lea	rcx, [rdx+rax]
+	mov	rax, QWORD PTR -72[rbp]     # a[i]
+	lea	rcx, [rdx+rax]              # rcx = &a[i]
 	mov	rax, QWORD PTR -24[rbp]
 	lea	rdx, 1[rax]
 	mov	QWORD PTR -24[rbp], rdx
 	mov	rdx, QWORD PTR -64[rbp]
-	add	rdx, rax
-	movzx	eax, BYTE PTR [rcx]
+	add	rdx, rax                    # c[cur_size]
+	movzx	eax, BYTE PTR [rcx]     # c[cur_size] = a[i];
 	mov	BYTE PTR [rdx], al
 .L7:
-	add	DWORD PTR -28[rbp], 1
+	add	DWORD PTR -28[rbp], 1       # cur_size++
 .L2:
-	mov	eax, DWORD PTR -28[rbp]
+	mov	eax, DWORD PTR -28[rbp]     # for (..; i < ..) {
 	movsx	rbx, eax
-	mov	rax, QWORD PTR -72[rbp]
+	mov	rax, QWORD PTR -72[rbp]     # a -> rax
 	mov	rdi, rax
 	call	strlen@PLT
-	cmp	rbx, rax
+	cmp	rbx, rax                    # for (..; i < strlen(a); ..) {
 	jb	.L8
-	mov	DWORD PTR -40[rbp], 0
+	mov	DWORD PTR -40[rbp], 0       # int i = 0 (второй внешний цикл)
+	                                # local i <=> -40[rbp]
 	jmp	.L9
 .L15:
-	mov	DWORD PTR -44[rbp], 0
-	mov	DWORD PTR -48[rbp], 0
+	mov	DWORD PTR -44[rbp], 0       # int found = 0
+	                                # local found <=> -44[rbp]
+	mov	DWORD PTR -48[rbp], 0       # int j = 0 (второй внутренний цикл)
+	                                # local j <=> -48[rbp]
 	jmp	.L10
 .L13:
 	mov	eax, DWORD PTR -48[rbp]
 	movsx	rdx, eax
 	mov	rax, QWORD PTR -64[rbp]
-	add	rax, rdx
+	add	rax, rdx                    # c[j]
 	movzx	edx, BYTE PTR [rax]
 	mov	eax, DWORD PTR -40[rbp]
 	movsx	rcx, eax
 	mov	rax, QWORD PTR -80[rbp]
-	add	rax, rcx
+	add	rax, rcx                    # b[i]
 	movzx	eax, BYTE PTR [rax]
-	cmp	dl, al
+	cmp	dl, al                      # if (c[j] == b[i]) {
 	jne	.L11
 	mov	DWORD PTR -44[rbp], 1
 	jmp	.L12
@@ -107,41 +118,43 @@ combination:
 .L10:
 	mov	eax, DWORD PTR -48[rbp]
 	cdqe
-	cmp	QWORD PTR -24[rbp], rax
+	cmp	QWORD PTR -24[rbp], rax    # for (int j = 0; j < cur_size; j++) {
 	ja	.L13
 .L12:
-	cmp	DWORD PTR -44[rbp], 0
+	cmp	DWORD PTR -44[rbp], 0       # if (!found) {
 	jne	.L14
 	mov	eax, DWORD PTR -40[rbp]
 	movsx	rdx, eax
 	mov	rax, QWORD PTR -80[rbp]
-	lea	rcx, [rdx+rax]
+	lea	rcx, [rdx+rax]              # rcx = &b[i]
 	mov	rax, QWORD PTR -24[rbp]
 	lea	rdx, 1[rax]
 	mov	QWORD PTR -24[rbp], rdx
 	mov	rdx, QWORD PTR -64[rbp]
 	add	rdx, rax
-	movzx	eax, BYTE PTR [rcx]
+	movzx	eax, BYTE PTR [rcx]     # c[cur_size++] = b[i];
 	mov	BYTE PTR [rdx], al
 .L14:
-	add	DWORD PTR -40[rbp], 1
+	add	DWORD PTR -40[rbp], 1       # cur_size++
 .L9:
-	mov	eax, DWORD PTR -40[rbp]
+	mov	eax, DWORD PTR -40[rbp]     # аналогичный цикл, но для второй строки
 	movsx	rbx, eax
 	mov	rax, QWORD PTR -80[rbp]
 	mov	rdi, rax
 	call	strlen@PLT
-	cmp	rbx, rax
+	cmp	rbx, rax                    # for (int i = 0; i < strlen(b); i++) {
 	jb	.L15
 	mov	rdx, QWORD PTR -64[rbp]
 	mov	rax, QWORD PTR -24[rbp]
 	add	rax, rdx
-	mov	BYTE PTR [rax], 0
+	mov	BYTE PTR [rax], 0           # c[cur_size] = '\0' - завершаем строку
 	mov	rax, QWORD PTR -64[rbp]
 	add	rsp, 72
 	pop	rbx
 	pop	rbp
 	ret
+
+
 	.size	combination, .-combination
 	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
--- input.s
+++ input.s
@@ -8,21 +8,27 @@ input:
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 48
-	mov	QWORD PTR -40[rbp], rdi
-	mov	QWORD PTR -8[rbp], 2
-	mov	QWORD PTR -16[rbp], 0
+	mov	QWORD PTR -40[rbp], rdi # inputStream
+	                            # inputStream <=> -40[rbp]
+	mov	QWORD PTR -8[rbp], 2    # size_t size = 2
+	                            # size <=> -8[rbp]
+	mov	QWORD PTR -16[rbp], 0   # size_t cur_size = 2
+	                            # cur_size <=> -16[rbp]
 	mov	rax, QWORD PTR -8[rbp]
 	mov	rdi, rax
 	call	malloc@PLT
-	mov	QWORD PTR -24[rbp], rax
+	mov	QWORD PTR -24[rbp], rax # char *str = (char *) malloc((size) * sizeof(char))
+	                            # str <=> -24[rbp]
 	mov	rax, QWORD PTR -24[rbp]
-	mov	BYTE PTR [rax], 0
+	mov	BYTE PTR [rax], 0       # str[0] = '\0'
 .L5:
 	mov	rax, QWORD PTR -40[rbp]
 	mov	rdi, rax
 	call	getc@PLT
-	mov	BYTE PTR -25[rbp], al
-	cmp	BYTE PTR -25[rbp], 10
+	mov	BYTE PTR -25[rbp], al   # char ch = (char) getc(inputStream)
+	                            # ch <=> -25[rbp]
+	cmp	BYTE PTR -25[rbp], 10   # if (..) - если символ завершения ввода
+	                            # то завершаем цикл:
 	jne	.L2
 	mov	rax, QWORD PTR stdin[rip]
 	cmp	QWORD PTR -40[rbp], rax
@@ -30,34 +36,36 @@ input:
 .L2:
 	cmp	BYTE PTR -25[rbp], -1
 	je	.L3
+
 	mov	rax, QWORD PTR -16[rbp]
 	add	rax, 1
-	cmp	QWORD PTR -8[rbp], rax
+	cmp	QWORD PTR -8[rbp], rax      # if (cur_size + 1 >= size) {
 	ja	.L4
 	sal	QWORD PTR -8[rbp]
-	mov	rdx, QWORD PTR -8[rbp]
+	mov	rdx, QWORD PTR -8[rbp]      # size *= 2
 	mov	rax, QWORD PTR -24[rbp]
 	mov	rsi, rdx
 	mov	rdi, rax
 	call	realloc@PLT
-	mov	QWORD PTR -24[rbp], rax
+	mov	QWORD PTR -24[rbp], rax     # str = realloc(str, size * sizeof(char))
 .L4:
 	mov	rax, QWORD PTR -16[rbp]
 	lea	rdx, 1[rax]
 	mov	QWORD PTR -16[rbp], rdx
 	mov	rdx, QWORD PTR -24[rbp]
 	add	rdx, rax
-	movzx	eax, BYTE PTR -25[rbp]
+	movzx	eax, BYTE PTR -25[rbp]  # str[cur_size++] = ch;
 	mov	BYTE PTR [rdx], al
-	jmp	.L5
+	jmp	.L5                         # бесконечный цикл (до конца ввода)
 .L3:
 	mov	rdx, QWORD PTR -24[rbp]
 	mov	rax, QWORD PTR -16[rbp]
 	add	rax, rdx
-	mov	BYTE PTR [rax], 0
+	mov	BYTE PTR [rax], 0           # str[cur_size] = '\0'
 	mov	rax, QWORD PTR -24[rbp]
 	leave
 	ret
+
 	.size	input, .-input
 	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
